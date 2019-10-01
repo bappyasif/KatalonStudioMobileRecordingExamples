aaf8adbed2476b2cd8dcde58ff402ee9e8d0fc76
BaPpY <bappyasif@hotmail.com>
2019-10-01 22:03:36 +0600

    Added To Initial Commit

---
+++ Test Suites/Regression Suites/Material Application Test Suite.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Create &amp; Login User Test.</description>
+   <name>Material Application Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>69149513-3a0f-40bc-b335-d59d2c8ea0a8</testSuiteGuid>
+   <testCaseLink>
+      <guid>72db59e2-15f2-4168-b6cf-d94e221d51ad</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Recording Mobile/Material Login/Create User Test Case</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d5f65d29-ffb2-476e-bfed-caceb0a20e53</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Recording Mobile/Material Login/User Login Test Case</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
